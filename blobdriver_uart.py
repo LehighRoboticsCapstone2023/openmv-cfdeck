0566ed11e75cc453257ff635c39c60694d2c6bed
Jiawei <mchlfitzgerald52@gmail.com>
2023-03-30 15:10:40 -0400

    Added comments, updated message format

--- blobdriver_uart.py
+++ blobdriver_uart.py
@@ -4,11 +4,13 @@ import math
 import sensor, image, time, pyb
 from pyb import UART
 
+# init camera sensor
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
+# init UART and timer
 clock = time.clock()
 uart = UART(1, 115200, timeout_char=100, parity=None)
 
@@ -22,20 +24,21 @@ while(True):
 
     blobs = img.find_blobs([threshold], areaThreshold=5000, merge=True) # detect blobs (red ball)
     maxBlobArea = 0
-    for blob in blobs:
+    for blob in blobs: # find largest blob
         img.draw_rectangle(blob.rect(), color=(0,0,255))
         x,y,w,h = blob.rect()
         area = w*h
         if (area >= MIN_AREA) and (area > maxBlobArea):
             maxBlobArea = area
 
-    speed = int(255 / math.sqrt(maxBlobArea)) if (maxBlobArea > 0) else 0
+    speed = int(255 / math.sqrt(maxBlobArea)) if (maxBlobArea > 0) else 0 # stupid speed formula (Z thrust)
     print(speed)
-    speed_bytes = speed.to_bytes(1, 'little')
+    #speed_bytes = speed.to_bytes(1, 'little')
 
     msg = bytearray(8)
-    msg[0] = 0x45
-    msg[1] = 1
-    msg[2] = speed
-    uart.write(msg)
-    time.sleep(0.1)
+    msg[0] = 0x69           # start bytes (the other end must synchronize to this pattern)
+    msg[1] = 0x69
+    msg[2] = 1              # "command" (arbitrary for now) - set Z velocity
+    msg[3] = speed          # write speed as one byte (quantized 0-255)
+    uart.write(msg)         # send 8 byte message
+    time.sleep(0.1)         # 100ms delay
